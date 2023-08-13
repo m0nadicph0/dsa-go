3b780f7c78caa55f01e413d64d198e7aac23bd88
m0nadicph0 <m0nadic.ph0@gmail.com>
2023-08-13 13:31:58 +0530

    clean up

--- main.go
+++ main.go
@@ -3,19 +3,3 @@ package main
 import "fmt"
 
 func main() {
-	RecPermute("", "")
-}
-
-func RecPermute(soFar string, rest string) {
-	fmt.Println("soFar:", soFar, ", rest:", rest)
-	if len(rest) == 0 {
-		fmt.Println(soFar)
-		return
-	}
-
-	for i := 0; i < len(rest); i++ {
-		next := soFar + string(rest[i])
-		remaining := string(rest[0:i]) + string(rest[i+1:])
-		RecPermute(next, remaining)
-	}
-}
